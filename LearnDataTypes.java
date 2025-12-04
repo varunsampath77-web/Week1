acd5e91dab1e287b23d5c3cc3be5484c736c72d3
varunsampath77-web <varunsampath77@gmail.com>
2025-12-04 14:44:29 +0530

    Add files via upload
    LearnDataTypes

---
+++ LearnDataTypes.java
@@ -0,0 +1,18 @@
+package example.test1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		int cod = 23;
+		
+		char car = 'C';
+		
+		String name = "Hem";
+		
+		boolean condition = true;
+		
+		System.out.println(condition);
+		
+	}
+
+}
