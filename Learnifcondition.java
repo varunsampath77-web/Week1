2c931f26663dd6496e9b76e7fcd5f1e0e6de4a47
Hemvarun <varunsampath77@gmail.com>
2025-12-05 09:38:05 +0530

    Add files via upload
    Learnifcondition

---
+++ Learnifcondition.java
@@ -0,0 +1,19 @@
+package example.test1;
+
+public class Learnifcondition {
+
+	public static void main(String[] args) {
+		int number = 0;
+		if(number>0) {
+			System.out.println("The number is positive");
+		}
+		else if(number<0) {
+			System.out.println("The number is negative");
+		}
+		else {
+			System.out.println("The number is null");
+		}
+	
+	}
+
+}
